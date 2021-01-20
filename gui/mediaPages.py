601d5b34cd396d36f6d783b1f740f39b94afd779
MS60 CCTu <CCTU@nuvoton.com>
2021-01-20 15:54:22 +0800

    added PyQt5 ui implementation to nuwriter
    Change-Id: I063eff0b3e8f962d0a566f5782062fa33170568e

---
+++ gui/mediaPages.py
@@ -0,0 +1,355 @@
+
+from PyQt5 import QtWidgets, QtCore
+
+from PyQt5.QtCore import QDateTime, Qt, QTimer
+from PyQt5.QtCore import pyqtSlot, pyqtSignal, QThread
+
+from PyQt5.QtWidgets import (QApplication, QCheckBox, QComboBox, QDateTimeEdit,
+        QDial, QDialog, QGridLayout, QGroupBox, QHBoxLayout, QLabel, QLineEdit,
+        QProgressBar, QPushButton, QRadioButton, QScrollBar, QSizePolicy,
+        QSlider, QSpinBox, QStyleFactory, QTableWidget, QTabWidget, QTextEdit,
+        QVBoxLayout, QFormLayout, QWidget)
+
+import os
+
+DEV_DDR_SRAM = 0
+DEV_NAND = 1
+DEV_SD_EMMC = 2
+DEV_SPINOR = 3
+DEV_SPINAND = 4
+DEV_OTP = 6
+DEV_USBH = 7
+DEV_UNKNOWN = 0xFF
+
+# class SdEmmcPage(QWidget):
+#     def __init__(self, media, parent=None):
+#         super(SdEmmcPage, self).__init__(parent)
+
+#         self._media = media
+
+
+#         self.mainLayout = QVBoxLayout()
+
+#         self.addWriteArgument()
+#         self.addReadArgument()
+#         self.addEraseArgument()
+
+#         self.mainLayout.addStretch()
+
+#         self.setLayout(self.mainLayout)
+#         self.addOptions()
+
+
+class SpiPage(QWidget):
+
+    signalImgProgram = pyqtSignal(int, str, str, int, bool)
+    signalImgRead = pyqtSignal(int, str, str, str, int, bool)
+    signalImgErase = pyqtSignal(int, str, str, int, bool)
+
+    def __init__(self, media, parent=None):
+        super(SpiPage, self).__init__(parent)
+
+        self.parent = parent
+        self._media = media
+
+
+        self.mainLayout = QVBoxLayout()
+
+        self.addWriteArgument()
+        self.addReadArgument()
+        self.addEraseArgument()
+
+        self.mainLayout.addStretch()
+
+        self.setLayout(self.mainLayout)
+        self.addOptions()
+
+        if parent != None:
+            self.signalImgRead.connect(parent.doImgRead)
+            self.signalImgProgram.connect(parent.doImgProgram)
+            self.signalImgErase.connect(parent.doImgErase)
+
+
+    def onRadioToggled(self):
+
+        if self.radioPack.isChecked():
+            self.imgAddress.setEnabled(False)
+        else:
+            self.imgAddress.setEnabled(True)
+
+    def addWriteArgument(self):
+        imgBrowseButton = QPushButton('Browse')
+        imgBrowseButton.clicked.connect(self.pathBrowse)
+
+        self.imgPathLine = QLineEdit('')
+
+        imgFileLayout = QHBoxLayout()
+        imgFileLayout.addWidget(self.imgPathLine)
+        imgFileLayout.addWidget(imgBrowseButton)
+
+        self.radioData = QRadioButton("Data")
+        self.radioPack = QRadioButton("Pack")
+
+        self.radioPack.toggled.connect(self.onRadioToggled)
+        self.radioPack.toggled.connect(self.onRadioToggled)
+
+        imgTypeLayout = QHBoxLayout()
+        imgTypeLayout.addWidget(self.radioData)
+        imgTypeLayout.addWidget(self.radioPack)
+        imgTypeLayout.addStretch()
+
+        self.imgAddress = QLineEdit('')
+
+        spiGroup = QGroupBox("Write")
+        spiLayout = QFormLayout()
+        spiLayout.addRow(QLabel("Image file"), imgFileLayout)
+        spiLayout.addRow(QLabel("Image type"), imgTypeLayout)
+        spiLayout.addRow(QLabel("Image addr."), self.imgAddress)
+
+        if self._media == DEV_NAND:
+            self.rawWrite = QCheckBox('OPT_RAW')
+            spiLayout.addRow(QLabel("Option"), self.rawWrite)
+
+
+
+
+
+        spiGroup.setLayout(spiLayout)
+        self.mainLayout.addWidget(spiGroup)
+
+    def addReadArgument(self):
+        group = QGroupBox("Read")
+        layout = QFormLayout()
+
+        _fileLayout = QHBoxLayout()
+        self.fileSave = QLineEdit('')
+        browseButton = QPushButton('Browse')
+        browseButton.clicked.connect(self.saveFile)
+        _fileLayout.addWidget(self.fileSave)
+        _fileLayout.addWidget(browseButton)
+
+        _rangeLayout = QHBoxLayout()
+        self.readStart = QLineEdit('')
+        self.readEnd = QLineEdit('')
+        self.readAll = QCheckBox('ALL')
+        self.readAll.setVisible(False)
+
+        self.readAll.stateChanged.connect(lambda checked: (self.readStart.setEnabled(not checked), self.readEnd.setEnabled(not checked)))
+
+        _rangeLayout.addWidget(self.readStart)
+        _rangeLayout.addWidget(QLabel("-"))
+        _rangeLayout.addWidget(self.readEnd)
+        _rangeLayout.addWidget(self.readAll)
+
+        layout.addRow(QLabel("Save file"), _fileLayout)
+        layout.addRow(QLabel("Range"), _rangeLayout)
+
+        group.setLayout(layout)
+        self.mainLayout.addWidget(group)
+
+    def addEraseArgument(self):
+        group = QGroupBox("Erase")
+        layout = QFormLayout()
+
+        _rangeLayout = QHBoxLayout()
+        self.eraseStart = QLineEdit('')
+        self.eraseEnd = QLineEdit('')
+        self.eraseAll = QCheckBox('ALL')
+
+        self.eraseAll.stateChanged.connect(lambda checked: (self.eraseStart.setEnabled(not checked), self.eraseEnd.setEnabled(not checked)))
+
+        _rangeLayout.addWidget(self.eraseStart)
+        _rangeLayout.addWidget(QLabel("-"))
+        _rangeLayout.addWidget(self.eraseEnd)
+        _rangeLayout.addWidget(self.eraseAll)
+
+        layout.addRow(QLabel("Range    "), _rangeLayout)
+
+        group.setLayout(layout)
+        self.mainLayout.addWidget(group)
+
+
+    def addOptions(self):
+        spiButtonLayout = QHBoxLayout()
+
+        programButton = QPushButton('Write')
+        programButton.clicked.connect(self.writeMedia)
+
+        readButton = QPushButton('Read')
+        readButton.clicked.connect(self.readMedia)
+
+        eraseButton = QPushButton('Erase')
+        eraseButton.clicked.connect(self.eraseMedia)
+
+        spiButtonLayout.addWidget(programButton)
+        spiButtonLayout.addWidget(readButton)
+        spiButtonLayout.addWidget(eraseButton)
+
+        spiButtonGroup = QGroupBox()
+        spiButtonGroup.setLayout(spiButtonLayout)
+
+        self.mainLayout.addWidget(spiButtonGroup)
+
+    def writeMedia(self):
+        _file = self.imgPathLine.text()
+        _address = self.imgAddress.text()
+        _media = self._media
+        _ispack = self.radioPack.isChecked()
+        self.signalImgProgram.emit(_media, _address , _file, 0, _ispack)
+
+    def readMedia(self):
+        _file = self.fileSave.text()
+        _start = self.readStart.text()
+        _end = self.readEnd.text()
+        _media = self._media
+        _isall = self.readAll.isChecked()
+        self.signalImgRead.emit(_media, _start , _file, _end, 0, _isall)
+
+    def eraseMedia(self):
+        _start = self.eraseStart.text()
+        _end = self.eraseEnd.text()
+        _media = self._media
+        _isall = self.eraseAll.isChecked()
+        self.signalImgErase.emit(_media, _start , _end, 0, _isall)
+
+
+    def pathBrowse(self):
+        filename = ""
+        # Fix for crash in X on Ubuntu 14.04
+        filename, _ = QtWidgets.QFileDialog.getOpenFileName()
+
+        if filename != "":
+            self.imgPathLine.setText(filename)
+
+        # if filename != "" and filename[-4:] in (".bin"):
+        #     self.imgPathLine.setText(filename)
+        # elif filename != "":
+        #     msgBox = QtWidgets.QMessageBox()
+        #     msgBox.setText("Wrong file extention. Must be .bin.")
+        #     msgBox.exec_()
+        # pass
+
+    def saveFile(self):
+        fileName, _ = QtWidgets.QFileDialog.getSaveFileName(self,
+                                    "save file",
+                                    os.getcwd(),
+                                    "All Files (*)")
+
+        if fileName != "":
+            self.fileSave.setText(fileName)
+            return
+
+class DdrSramPage(QWidget):
+
+    signalImgProgram = pyqtSignal(int, str, str, int, bool)
+
+    def __init__(self, parent=None):
+        super(DdrSramPage, self).__init__(parent)
+
+        self.imgPathLine = QLineEdit('')
+        self.imgAddress = QLineEdit('1000')
+        self.optExecute = QCheckBox('Execute after download')
+
+        imgFileLayout = QHBoxLayout()
+        imgFileLayout.addWidget(self.imgPathLine)
+        # imgFileLayout.addWidget(QPushButton('Browse'))
+        imgBrowseButton = QPushButton('Browse')
+        imgBrowseButton.clicked.connect(self.pathBrowse)
+        imgFileLayout.addWidget(imgBrowseButton)
+
+        optionLayout = QHBoxLayout()
+        optionLayout.addWidget(self.optExecute)
+        optionLayout.addStretch()
+
+        progGroup = QGroupBox("Parameters")
+        progLayout = QFormLayout()
+        progLayout.addRow(QLabel("Image file"), imgFileLayout)
+        progLayout.addRow(QLabel("Image addr."), self.imgAddress)
+
+        progLayout.addRow(QLabel("Option"), optionLayout)
+
+        progGroup.setLayout(progLayout)
+
+        self.mainLayout = QVBoxLayout()
+        self.mainLayout.addWidget(progGroup)
+        self.mainLayout.addStretch()
+
+        self.setLayout(self.mainLayout)
+        self.addOptions()
+
+    def pathBrowse(self):
+        filename = ""
+        # Fix for crash in X on Ubuntu 14.04
+        filename, _ = QtWidgets.QFileDialog.getOpenFileName()
+        if filename != "":
+            self.imgPathLine.setText(filename)
+        pass
+
+    def addOptions(self):
+        optionLayout = QHBoxLayout()
+
+        programButton = QPushButton('Write')
+        programButton.clicked.connect(self.writeDDR)
+
+        # optionLayout.addStretch()
+        optionLayout.addWidget(programButton)
+        # optionLayout.addStretch()
+
+        optionGroup = QGroupBox()
+        optionGroup.setLayout(optionLayout)
+
+        self.mainLayout.addWidget(optionGroup)
+
+    def writeDDR(self):
+        imgFile = self.imgPathLine.text()
+        # With the 0x prefix, Python can distinguish hex and decimal automatically.
+        # imgAddr = int(self.imgAddress.text(),0)
+        imgAddr = self.imgAddress.text()
+
+        # Command options
+        # OPT_NONE = 0
+        # OPT_EXECUTE = 2     # For write
+        if self.optExecute.isChecked():
+            option = 2
+        else:
+            option = 0
+
+        # DEV_DDR_SRAM = 0
+        media = 0
+        self.signalImgProgram.emit(media, imgAddr , imgFile, option, False)
+
+if __name__ == "__main__":
+    class MainDialog(QDialog):
+        def __init__(self, parent=None):
+            super(MainDialog, self).__init__(parent)
+            self.tabMedia = QTabWidget()
+
+            # DDR/SRAM
+            self.ddrPage = DdrSramPage()
+            self.ddrPage.signalImgProgram.connect(self.doImgProgram)
+            self.tabMedia.addTab(self.ddrPage, "DDR/SRAM")
+
+            # NAND
+            self.nandPage = SpiPage(DEV_NAND)
+            self.tabMedia.addTab(self.nandPage, "NAND")
+            self.nandPage.signalImgProgram.connect(self.doImgProgram)
+            self.tabMedia.setCurrentIndex(1)
+
+
+            mainLayout = QVBoxLayout()
+            mainLayout.setContentsMargins(5, 5, 5, 5)
+            # addWidget
+            mainLayout.addWidget(self.tabMedia)
+            self.setLayout(mainLayout)
+            self.setWindowTitle("MA35D1 NuWriter")
+
+        @QtCore.pyqtSlot(int, str, str, int, bool)
+        def doImgProgram(self, media, start, image_file_name, option, ispack):
+            print(f"slot doImgProgram {media}, {start}, {image_file_name}, {option}, {ispack}")
+
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    myapp = MainDialog()
+    myapp.show()
+    sys.exit(app.exec_())
---
+++ nuwriterUI.py
@@ -0,0 +1,394 @@
+
+from PyQt5 import QtWidgets, QtCore, QtGui
+import sys
+import os
+
+# FIXME: without dispose_resources, the ui tool raises an exception.
+# SystemExit: [Errno 13] Access denied (insufficient permissions)
+# but, with dispose_resources, the command tool raises timeout error.
+
+# from nuwriter import *
+from nuwriterTEMP import *
+
+class EmittingStream(QtCore.QObject):
+
+    textWritten = QtCore.pyqtSignal(str)
+
+    def write(self, text):
+        self.textWritten.emit(str(text))
+
+from mainwindow import Ui_MainWindow
+from gui.mediaPages import DdrSramPage, SpiPage
+
+try:
+    # Include in try/except block if you're also targeting Mac/Linux
+    from PyQt5.QtWinExtras import QtWin
+    myappid = 'Nuvoton.nuwriter.UiPyqt5.0120'
+    QtWin.setCurrentProcessExplicitAppUserModelID(myappid)
+except ImportError:
+    pass
+
+import configparser
+
+class Ui(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self) # Call the inherited classes __init__ method
+        # uic.loadUi('gui/ui/NUA3500-Writer.ui', self) # Load the .ui file
+
+        self.setupUi(self)
+
+
+        self.setWindowTitle("MA35D1 NuWriter")
+
+        self.addMedia()
+
+        self.text_browser = QtWidgets.QTextBrowser(self)
+        self.verticalLayout.addWidget(self.text_browser)
+
+        # Install the custom output stream
+        # sys.stdout = EmittingStream(textWritten=self.normalOutputWritten)
+        self.outputStream = EmittingStream(textWritten=self.normalOutputWritten)
+        sys.stdout = self.outputStream
+        sys.stderr = self.outputStream
+
+        self.initToolSetting()
+
+        # Attach
+        self.browseDDR_btn.clicked.connect(self.iniBrowse)
+        self.attach_btn.clicked.connect(self.doAttach)
+
+    def addMedia(self):
+
+        # DEV_DDR_SRAM = 0
+        self.ddrPage = DdrSramPage()
+        self.tabMedia.addTab(self.ddrPage, "DDR/SRAM")
+        self.ddrPage.signalImgProgram.connect(self.doImgProgram)
+
+        # DEV_NAND = 1
+        self.nandPage = SpiPage(DEV_NAND, self)
+        self.tabMedia.addTab(self.nandPage, "NAND")
+
+        # DEV_SD_EMMC = 2
+        self.sdEmmcPage = SpiPage(DEV_SD_EMMC, self)
+        self.tabMedia.addTab(self.sdEmmcPage, "SD/EMMC")
+
+        # DEV_SPINOR = 3
+        self.spiNorPage = SpiPage(DEV_SPINOR, self)
+        self.tabMedia.addTab(self.spiNorPage, "SPI NOR")
+
+        # DEV_SPINAND = 4
+        self.spiNandPage = SpiPage(DEV_SPINAND, self)
+        self.tabMedia.addTab(self.spiNandPage, "SPI NAND")
+
+        # DEV_OTP = 6
+
+
+    def __del__(self):
+        # Restore sys.stdout
+        sys.stdout = sys.__stdout__
+        sys.stderr = sys.__stderr__
+
+    ################################################################################################
+    # 'MA35D1.ini'
+    ################################################################################################
+
+    def initToolSetting(self):
+
+        self.iniFileName = 'MA35D1.ini'
+
+        iniFileName = self.iniFileName
+
+        if not os.path.exists(iniFileName):
+            open(iniFileName, 'w', encoding='utf-8')
+
+        self.conf = configparser.ConfigParser()
+        self.conf.read(iniFileName, encoding='utf-8')
+
+        section = 'Attach'
+
+        if not self.conf.has_section(section):
+            self.conf.add_section(section)
+            self.conf.set(section, 'Ini File', '')
+        else:
+            self.ddrFileLineEdit.setText(self.conf.get(section, 'Ini File', fallback=''))
+
+        section = 'DDR'
+
+        if not self.conf.has_section(section):
+            self.conf.add_section(section)
+            self.conf.set(section, 'write file', '')
+            self.conf.set(section, 'write addr', '')
+            self.conf.set(section, 'write execute', 'false')
+        else:
+            self.ddrPage.imgPathLine.setText(self.conf.get(section, 'write file', fallback=''))
+            self.ddrPage.imgAddress.setText(self.conf.get(section, 'write addr', fallback=''))
+            self.ddrPage.optExecute.setChecked(self.conf.getboolean(section, 'write execute', fallback=False))
+
+
+        sections = ['NAND', 'SPINOR', 'SPINAND', 'SD']
+
+        for sec in sections:
+            if sec == 'NAND':
+                page = self.nandPage
+            elif sec == 'SPINOR':
+                page = self.spiNorPage
+            elif sec == 'SPINAND':
+                page = self.spiNandPage
+            elif sec == 'SD':
+                page = self.sdEmmcPage
+
+            if not self.conf.has_section(sec):
+                self.conf.add_section(sec)
+                self.conf.set(sec, 'write file', '')
+                self.conf.set(sec, 'write addr', '')
+                self.conf.set(sec, 'write pack', 'false')
+                self.conf.set(sec, 'read file', '')
+                self.conf.set(sec, 'read start', '')
+                self.conf.set(sec, 'read length', '')
+                self.conf.set(sec, 'read all', 'false')
+                self.conf.set(sec, 'erase start', '')
+                self.conf.set(sec, 'erase length', '')
+                self.conf.set(sec, 'erase all', 'false')
+            else:
+                page.imgPathLine.setText(self.conf.get(sec, 'write file', fallback=''))
+                page.imgAddress.setText(self.conf.get(sec, 'write addr', fallback=''))
+                page.radioPack.setChecked(self.conf.getboolean(sec, 'write pack', fallback=False))
+                page.fileSave.setText(self.conf.get(sec, 'read file', fallback=''))
+                page.readStart.setText(self.conf.get(sec, 'read start', fallback=''))
+                page.readEnd.setText(self.conf.get(sec, 'read length', fallback=''))
+                page.readAll.setChecked(self.conf.getboolean(sec, 'read all', fallback=False))
+                page.eraseStart.setText(self.conf.get(sec, 'erase start', fallback=''))
+                page.eraseEnd.setText(self.conf.get(sec, 'erase length', fallback=''))
+                page.eraseAll.setChecked(self.conf.getboolean(sec, 'erase all', fallback=False))
+
+    def closeEvent(self, evt):
+        # self.conf.write(open(self.iniFileName, 'w', encoding='utf-8'))
+        pass
+
+    def normalOutputWritten(self, text):
+        """Append text to the QTextEdit."""
+        # Maybe QTextEdit.append() works as well, but this is how I do it:
+        self.text_browser.insertPlainText(text)
+
+    def iniBrowse(self):
+        filename = ""
+        # Fix for crash in X on Ubuntu 14.04
+        filename, _ = QtWidgets.QFileDialog.getOpenFileName()
+        if filename != "":
+            self.ddrFileLineEdit.setText(filename)
+
+    ################################################################################################
+    # command line
+    ################################################################################################
+
+    @QtCore.pyqtSlot()
+    def doAttach(self):
+        iniFile = self.ddrFileLineEdit.text()
+        self.conf.set('Attach', 'Ini File', iniFile)
+        self.conf.write(open(self.iniFileName, 'w', encoding='utf-8'))
+
+        self.text_browser.clear()
+        print(f'do_attach({iniFile})')
+
+        try:
+            do_attach(iniFile)
+        except SystemExit as e:
+            print(f'SystemExit: {e}')
+            pass
+        except Exception as e:
+            print(f'An Error occurred: {e}')
+            pass
+        except:
+            print('except')
+            pass
+
+    # def do_img_read(media, start, out_file_name, length=0x1, option=OPT_NONE) -> None:
+    @QtCore.pyqtSlot(int, str, str, str, int, bool)
+    def doImgRead(self, media, startStr, fileStr, lengthStr, option, isall=False):
+
+        if isall:
+            start = 0
+            length = 0
+        else:
+            try:
+                start = int(startStr, 0) & 0xffffffff
+            except:
+                start = 0
+
+            try:
+                length = int(lengthStr, 0) & 0xffffffff
+            except:
+                length = 0x1
+
+
+        self.text_browser.clear()
+
+        if media in [DEV_NAND, DEV_SPINOR, DEV_SPINAND, DEV_SD_EMMC]:
+
+            if media == DEV_NAND:
+                section = 'NAND'
+            elif media == DEV_SPINOR:
+                section = 'SPINOR'
+            elif media == DEV_SPINAND:
+                section = 'SPINAND'
+            else:
+                section = 'SD'
+
+            self.conf.set(section, 'read file', fileStr)
+            self.conf.set(section, 'read start', startStr)
+            self.conf.set(section, 'read length', lengthStr)
+
+            if isall:
+                self.conf.set(section, 'read all', 'true')
+            else:
+                self.conf.set(section, 'read all', 'false')
+
+        self.conf.write(open(self.iniFileName, 'w', encoding='utf-8'))
+
+        try:
+            print(f'do_img_read({media}, {start}, {fileStr}, {length}, {option})')
+            do_img_read(media, start, fileStr, length, option)
+        except SystemExit as e:
+            print(f'SystemExit: {e}')
+            pass
+        except Exception as e:
+            print(f'An Error occurred: {e}')
+            pass
+        except:
+            print('except')
+            pass
+
+
+        pass
+
+    # def do_img_program(media, start, image_file_name, option=OPT_NONE) -> None:
+    @QtCore.pyqtSlot(int, str, str, int, bool)
+    def doImgProgram(self, media, startStr, image_file_name, option, ispack=False):
+
+        try:
+            start = int(startStr, 0) & 0xffffffff
+        except:
+            start = 0
+
+        self.text_browser.clear()
+
+        if media == DEV_DDR_SRAM:
+            section = 'DDR'
+            self.conf.set(section, 'write file', image_file_name)
+            self.conf.set(section, 'write addr', startStr)
+            if option == 0:
+                self.conf.set(section, 'write execute', 'false')
+            elif option == 2:
+                self.conf.set(section, 'write execute', 'true')
+
+        elif media in [DEV_NAND, DEV_SPINOR, DEV_SPINAND, DEV_SD_EMMC]:
+
+            if media == DEV_NAND:
+                section = 'NAND'
+            elif media == DEV_SPINOR:
+                section = 'SPINOR'
+            elif media == DEV_SPINAND:
+                section = 'SPINAND'
+            else:
+                section = 'SD'
+
+            self.conf.set(section, 'write file', image_file_name)
+            self.conf.set(section, 'write addr', startStr)
+
+            if ispack:
+                self.conf.set(section, 'write pack', 'true')
+            else:
+                self.conf.set(section, 'write pack', 'false')
+
+        elif media == DEV_OTP:
+            section = 'OTP'
+            self.conf.set(section, 'write file', image_file_name)
+
+        self.conf.write(open(self.iniFileName, 'w', encoding='utf-8'))
+
+        try:
+            if media == DEV_OTP:
+                print(f'do_otp_program({image_file_name})')
+                do_otp_program(image_file_name)
+            elif ispack:
+                print(f'do_otp_program({media}, {image_file_name}, {option})')
+                do_pack_program(media, image_file_name, option)
+            else:
+                print(f'do_img_program({media}, {start}, {image_file_name}, {option})')
+                do_img_program(media, start, image_file_name, option)
+        except SystemExit as e:
+            print(f'SystemExit: {e}')
+            pass
+        except Exception as e:
+            print(f'An Error occurred: {e}')
+            pass
+        except:
+            print('except')
+            pass
+
+    # def do_img_erase(media, start, length=0, option=OPT_NONE) -> None:
+    @QtCore.pyqtSlot(int, str, str, int, bool)
+    def doImgErase(self, media, startStr, lengthStr, option, isall=False):
+
+        if isall:
+            start = 0
+            length = 0
+        else:
+            try:
+                start = int(startStr, 0) & 0xffffffff
+            except:
+                start = 0
+
+            try:
+                length = int(lengthStr, 0) & 0xffffffff
+            except:
+                length = 0x1
+
+
+        self.text_browser.clear()
+
+        if media in [DEV_NAND, DEV_SPINOR, DEV_SPINAND, DEV_SD_EMMC]:
+
+            if media == DEV_NAND:
+                section = 'NAND'
+            elif media == DEV_SPINOR:
+                section = 'SPINOR'
+            elif media == DEV_SPINAND:
+                section = 'SPINAND'
+            else:
+                section = 'SD'
+
+            self.conf.set(section, 'erase start', startStr)
+            self.conf.set(section, 'erase length', lengthStr)
+
+            if isall:
+                self.conf.set(section, 'erase all', 'true')
+            else:
+                self.conf.set(section, 'erase all', 'false')
+
+        self.conf.write(open(self.iniFileName, 'w', encoding='utf-8'))
+
+        try:
+            print(f'do_img_erase({media}, {start}, {length}, {option})')
+            do_img_erase(media, start, length, option)
+        except SystemExit as e:
+            print(f'SystemExit: {e}')
+            pass
+        except Exception as e:
+            print(f'An Error occurred: {e}')
+            pass
+        except:
+            print('except')
+            pass
+
+
+        pass
+
+if __name__ == "__main__":
+
+    app = QtWidgets.QApplication(sys.argv)
+    # tool icon
+    app.setWindowIcon(QtGui.QIcon(':/icons/app.ico'))
+    myapp = Ui()
+    myapp.show()
+    sys.exit(app.exec_())
